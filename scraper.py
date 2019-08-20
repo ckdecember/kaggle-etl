265ecb53256b87cf7e9234df3df764d0b743d2e9
carroll kong <github@carrollkong.com>
2019-08-20 15:00:55 -0400

    trying to add BACK beautiful soup

--- scraper.py
+++ scraper.py
@@ -9,28 +9,39 @@ Use pycurl???  or not
 """
 
 import argparse
-import itertools
-import json
 import logging
-import time
-import urllib.request
-import urllib.parse
+import os
 
+from bs4 import BeautifulSoup, SoupStrainer
+from dotenv import load_dotenv
 import pycurl
 
+load_dotenv()
+
 class WebScrape():
-    def readurl(self, host_url):
+    def get_url(self, host_url):
+        """ reads the url given """
         c = pycurl.Curl()
         c.setopt(c.URL, host_url)
         c.setopt(pycurl.TIMEOUT, 10)
-
         c.setopt(pycurl.FOLLOWLOCATION, 1)
-        #c.setopt(pycurl.POSTFIELDS, 'username={user}&password={password}'.format(**host_login))
-        # X-XSRF-TOKEN
+        c.setopt(pycurl.SSL_VERIFYPEER, 0);
+        session = c
+        return session
+
+    def readpage(self):
+        # read the webpage dom for this token
+        # input name=X-XSRF-TOKEN
+        # use bs4 to read a little bit.  get the token tag.  refeed tag.
+        pass
+
+    def readstuff(self):
+        pass
+        user = os.environ['USERNAME']
+        password = os.environ['PASSWORD']
+        c.setopt(pycurl.POSTFIELDS, 'username={}&password={}'.format(user, password))
         #c.setopt(pycurl.COOKIEJAR, 'data/kaggle.cookie')
-
         # c.setopt(c.VERBOSE, True)
-
         c.setopt(pycurl.SSL_VERIFYPEER, 0);
         session = c
         return session
@@ -38,7 +49,7 @@ class WebScrape():
 def main():
     print ("Web Scraper Module")
     ws = WebScrape()
-    session = ws.readurl("https://www.kaggle.com/account/login?phase=emailSignIn&returnUrl=%2Fwendykan%2Flending-club-loan-data%2Fversion%2F1")
+    session = ws.get_url("https://www.kaggle.com/account/login?phase=emailSignIn&returnUrl=%2Fwendykan%2Flending-club-loan-data%2Fversion%2F1")
     print (session)
     session.perform()
 
